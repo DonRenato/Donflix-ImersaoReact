dc22fb7c8db7faf59256badbddedeee16c83992c
Renato  Avellar <onizuka@Mac-mini.local>
2020-07-28 19:01:46 -0300

    criando form

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PageDefault from '../../../Components/PageDefault';
+import {Link} from 'react-router-dom';
 
 
 function CadastroCategoria(){
     return(
         <PageDefault>
-            
-            
-            
-            
-            
-            
-            <h1>Página de cadastro de videos</h1>
+         <h1>Cadastro de Categoria</h1>
+
+<form>
+
+  <label>
+    Nome da Categoria:
+    <input
+      type="text"
+    />
+  </label>
+
+  <button>
+    Cadastrar
+  </button>
+</form>
+
+
+<Link to="/">
+  Ir para home
+</Link>
 
       
         </PageDefault>
