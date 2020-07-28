026fb82c3d7d892e9fd2f204d01c128b475a2582
Renato  Avellar <onizuka@Mac-mini.local>
2020-07-28 15:59:39 -0300

    atualizando estilos

--- src/Components/BannerMain/styles.js
+++ src/Components/BannerMain/styles.js
@@ -35,6 +35,7 @@ ContentAreaContainer.Title = styled.h2`
   line-height: 1;
   margin-top: 0;
   margin-bottom: 32px;
+  
   @media (max-width: 800px) {
     font-size: 32px;
     text-align: center;
@@ -47,12 +48,34 @@ export const BannerMainContainer = styled.section`
   background-image: ${({backgroundImage}) => `url(${backgroundImage})`};
   background-size: cover;
   background-position: center;
+ 
 
   @media (max-width: 800px){
     height: auto;
     min-height: 50vh;
 
   }
+
+  &:after,
+  &:before {
+    content: "";
+    display: block;
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin: auto;
+    height: 20%;
+  }
+  &:before {
+    top: 0;
+    height: 100%;
+    background: rgba(0,0,0,0.5);
+  }
+  &:after {
+    bottom: 0;
+    background: linear-gradient(0deg, #141414 0%, transparent 100%);
+  }
+  
  
 `;
 
