6b3b13d28fc4c1f875c6bd994accab7b752a3722
Renato  Avellar <onizuka@Mac-mini.local>
2020-07-31 23:56:57 -0300

    agora foi!!!!!!

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -97,6 +97,10 @@ function CadastroCategoria() {
           // eslint-disable-next-line react/no-array-index-key
           <li key={`${category.id}`}>
             {category.name}
+            {' '}
+            -
+            {' '}
+            {category.description}
           </li>
 
         ))}
