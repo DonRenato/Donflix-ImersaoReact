ca635d26514da4e6438132d42a0f23e043b13360
Renato  Avellar <onizuka@Mac-mini.local>
2020-07-31 23:45:18 -0300

    sera q vai?

--- src/Pages/Cadastro/Categoria/index.js
+++ src/Pages/Cadastro/Categoria/index.js
@@ -96,7 +96,7 @@ function CadastroCategoria() {
         {categories.map((category) => (
           // eslint-disable-next-line react/no-array-index-key
           <li key={`${category.id}`}>
-            {category.name}
+            {category.title}
           </li>
 
         ))}
