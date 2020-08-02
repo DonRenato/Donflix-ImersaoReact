c9b4acfc54dd08c98f95610a4e9480624f0d912f
Renato  Avellar <onizuka@Mac-mini.local>
2020-08-02 17:27:59 -0300

    conectando server

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ const URL_BACKEND_2 = window.location.hostname.includes('hostname')
   ? 'http://localhost:8080'
   : 'https://donflix.herokuapp.com';
 
-const URL_BACKEND = 'http://localhost:8080';
+// const URL_BACKEND = 'http://localhost:8080';
 export default {
-  URL_BACKEND,
+  URL_BACKEND_2,
 };
